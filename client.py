498273b9426be2b2a356aa1d47d482c504e19960
Tavrovskiy <Alex_Tav@icloud.com>
2019-12-26 21:07:20 +0300

    vse_i_dop

--- client.py
+++ client.py
@@ -1,5 +1,23 @@
 import socket
 import pickle
+from random import randint
+
+
+#шифрование
+def shifr(cript, key): 
+    c = [chr(ord(cript[i]) ^ key) for i in range(len(cript))]
+    return ''.join(c)
+
+#отправка
+def send(conn, msg, key):
+    msg = shifr(msg, key)
+    conn.send(pickle.dumps(msg))
+
+#дешифровка
+def de_shifr(conn, key):
+    msg = pickle.loads(conn.recv(1024))
+    msg = shifr(msg, key)
+    return msg
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -7,8 +25,20 @@ PORT = 8080
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
+g = randint(0,100)
+p = randint(0,100)
+a = randint(0,100)
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+B = pickle.loads(sock.recv(1024))
+key = B ** a % p
+print('Enter your message:')
+msg = input()
+
+while msg != 'exit':
+     print(f"Send to server:\n message {msg},\n key: {key}")
+     send(sock,msg, key)
+     print(de_shifr(sock,key))
+     msg = input()
 
 sock.close()
--- server.py
+++ server.py
@@ -1,5 +1,34 @@
 import socket
 import pickle
+from random import randint
+
+
+#шифрование
+def shifr(cript, key): 
+    c = [chr(ord(cript[i]) ^ key) for i in range(len(cript))]
+    return ''.join(c)
+
+#отправка
+def send(conn, msg, key):
+    msg = shifr(msg, key)
+    conn.send(pickle.dumps(msg))
+
+#дешифровка
+def de_shifr(conn, key):
+    msg = pickle.loads(conn.recv(1024))
+    msg = shifr(msg, key)
+    return msg
+
+def check(key):
+
+     with open('key.txt', 'r') as file:
+         f = False
+         for line in file:
+             if int(line) == key:
+                 f = True
+                 break
+     return f
+
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -11,5 +40,25 @@ conn, addr = sock.accept()
 
 msg = conn.recv(1024)
 print(pickle.loads(msg))
+p, g, A = pickle.loads(msg)
+b = randint(0, 300)
+B = g ** b % p
+conn.send(pickle.dumps(B))
+key = A ** b % p
+
+while True:
+    try:
+        f = check(key)
+        if f == False:
+                print("incorrect key");
+                send(conn, "incorrect key", key)
+                break
+        msg = de_shifr(conn, key)
+        print(f"From client:\n{conn},\n{key}\n")
+        print(f"To client:\n{msg}")
+        send(conn, msg, key)
+
+    except EOFError:
+        break
 
 conn.close()
